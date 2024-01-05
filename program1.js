5f8f263142256a4c73a57c588e8f71ce4956d7bc
Yadvnisha7720 <yadavnisha7720@gmail.com>
2024-01-05 13:42:31 +0530

    ok

--- program1.js
+++ program1.js
@@ -2,10 +2,32 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
+//var isValid = function(s) {
     
-};
+//};
 
-module.exports = { isValid };
+//module.exports = { isValid };
 
 
+function isValidParentheses(input) {
+    const stack = [];
+    const parenthesesMap = { '(': ')', '[': ']', '{': '}' };
+  
+    for (let char of input) {
+      if (char in parenthesesMap) {
+        stack.push(char);
+      } else if (Object.values(parenthesesMap).includes(char)) {
+        if (char !== parenthesesMap[stack.pop()]) {
+          return false;
+        }
+      }
+    }
+  
+    return stack.length === 0;
+  }
+  
+  // Example usage:
+  const userInput = prompt('Enter a string with parentheses:');
+  const result = isValidParentheses(userInput);
+  
+  console.log(result);
